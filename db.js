b74b1db6d0b9f5f06b7c0b52540a797b60a70265
Muhammed Hasan <Muhammedahasan@outlook.com>
2018-12-14 23:28:44 +0000

    Add file to handle db connection.

---
+++ db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+let connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'express',
+  password : 'password',
+  database : 'express_cms'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
