680899c5025a1d4bbbcec22ddfcc9cad01df22bf
Muhammed Hasan <Muhammedahasan@outlook.com>
2018-12-14 23:29:06 +0000

    Add router, models and index file.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const portNumber = 3000;
+const pageRouter = require('./routers/page')
+
+app.use('/pages', pageRouter)
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(portNumber, () => console.log('Running on port ' + portNumber));
