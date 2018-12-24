eb8026f7b136da7af27ea34ba16393d481446ffc
Muhammed Hasan <Muhammedahasan@outlook.com>
2018-12-24 13:21:36 +0000

    Stop using postgres.

--- db.js
+++ db.js
@@ -1,29 +1,20 @@
 const Sequelize = require('sequelize');
-const express = require('express');
-const app = express();
 const database = 'express_cms';
 const user = 'express';
 const password = 'password';
-let sequelize = {};
 
-if (app.settings.env === 'development') {
-    sequelize = new Sequelize(database, user, password, {
-        host: 'localhost',
-        dialect: 'mysql',
-        operatorsAliases: false,
-    
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-    });
-} else {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
-    });
-}
+const sequelize = new Sequelize(database, user, password, {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
 
 sequelize
     .authenticate()
