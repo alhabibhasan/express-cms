5a203f13b896bbaf820b0ac7e5861ed6c2a15b31
Muhammed Hasan <Muhammedahasan@outlook.com>
2018-12-24 13:08:37 +0000

    Add postgres support for production.

--- db.js
+++ db.js
@@ -1,20 +1,27 @@
 const Sequelize = require('sequelize');
+const express = require('express');
+const app = express();
 const database = 'express_cms';
 const user = 'express';
 const password = 'password';
+const sequelize = {};
 
-const sequelize = new Sequelize(database, user, password, {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false,
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-});
+if (app.settings.env === 'developement') {
+    sequelize = new Sequelize(database, user, password, {
+        host: 'localhost',
+        dialect: 'mysql',
+        operatorsAliases: false,
+    
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+    });
+} else {
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+}
 
 sequelize
     .authenticate()
